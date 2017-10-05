0b02f97adaf0fb27093ca30c4752c440267a512b
matthewdking <matthew.d.king@outlook.com>
2017-10-05 16:44:45 +0300

    creates index.js relates #17

---
+++ exercise-1/index.js
@@ -0,0 +1,9 @@
+const psst = require('./psst.js');
+const {
+  sign,
+  validate
+} = psst('super string');
+
+// call the sign function and log the response
+
+// call the validate function and log the response
