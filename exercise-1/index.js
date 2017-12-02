8fdc95423e77da1d7e3acf8e65ec15bd4ac94c67
matthewdking <matthew.d.king@outlook.com>
2017-12-02 22:07:35 +0200

    fixes ex1 to use hex and updates readme #23 :lock:

--- exercise-1/index.js
+++ exercise-1/index.js
@@ -1,8 +1,10 @@
 const psst = require('./psst.js');
-const {
-  sign,
-  validate
-} = psst('super string');
+const { sign, validate } = psst('super string');
 
 console.log(sign('Mario'));
-console.log(validate('Mario','aBUp4wGxq4muyS9ZfABdcmOO4NC7GbHjy63Yqf68Re8='));
+console.log(
+  validate(
+    'Mario',
+    '681529e301b1ab89aec92f597c005d72638ee0d0bb19b1e3cbadd8a9febc45ef'
+  )
+);
--- exercise-1/psst.js
+++ exercise-1/psst.js
@@ -1,18 +1,18 @@
 'use strict';
 
-module.exports = (secret) => {
+module.exports = secret => {
   if (!secret || typeof secret !== 'string') {
     throw Error('invalid secret!');
   }
 
   const functions = {
-    sign: (value) => {
+    sign: value => {
       return 'oops';
     },
     validate: (value, hash) => {
       return false;
-    }
+    },
   };
 
   return functions;
-}
+};
