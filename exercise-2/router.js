9d992b01919c5836205f9f950520e71f03b6b3f4
matt lubel <matt_lubel@hotmail.com>
2018-12-05 11:15:36 +0200

    make ex2 formatting more like standard js formatting

--- exercise-2/router.js
+++ exercise-2/router.js
@@ -7,29 +7,29 @@ const jwt = require('jsonwebtoken');
 const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
 
 module.exports = (req, res) => {
+  // simple request log
+  console.log(`${req.method} ${req.url}`)
+
   switch (`${req.method} ${req.url}`) {
-    case 'GET /':
-      return readFile(
-        './index.html',
-        (err, data) => {
-          res.writeHead(
-            200,
-            {
-              'Content-Type': 'text/html',
-              'Content-Length': data.length
-            }
-          );
-          return res.end(data);
-        }
-      );
-    default:
-      res.writeHead(
-        404,
-        {
+
+    case 'GET /': {
+      return readFile('./index.html', (err, data) => {
+        res.writeHead(200, {
           'Content-Type': 'text/html',
-          'Content-Length': notFoundPage.length
-        }
-      );
+          'Content-Length': data.length
+        });
+        return res.end(data);
+      });
+    }
+
+    // Add more cases here
+
+    default: {
+      res.writeHead(404, {
+        'Content-Type': 'text/html',
+        'Content-Length': notFoundPage.length
+      });
       return res.end(notFoundPage);
+    }
   }
 }
--- solutions/2/router.js
+++ solutions/2/router.js
@@ -10,83 +10,79 @@ const userDetails = { userId: 5, role: 'admin' };
 
 const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
 
-module.exports = (req, res) => {
-  switch (`${req.method} ${req.url}`) {
-    case 'GET /':
-      return readFile(
-        './index.html',
-        (err, data) => {
-          res.writeHead(
-            200,
-            {
-              'Content-Type': 'text/html',
-              'Content-Length': data.length
-            }
-          );
-          return res.end(data);
-        }
-      );
-    case 'POST /login':
-      const cookie = sign(userDetails, SECRET);
-      res.writeHead(
-        302,
-        {
-          'Location': '/',
-          'Set-Cookie': `jwt=${cookie}; HttpOnly`
-        }
-      );
-      return res.end();
-    case 'POST /logout':
-      res.writeHead(
-        302,
-        {
-          'Location': '/',
-          'Set-Cookie': 'jwt=0; Max-Age=0'
-        }
-      );
-      return res.end();
-    case 'GET /auth_check':
-      const send401 = () => {
-        const message = 'fail!';
-        res.writeHead(
-          401,
-          {
-            'Content-Type': 'text/plain',
-            'Content-Length': message.length
-          }
-        );
-        return res.end(message);
-      }
+const send401 = res => {
+  const message = 'fail!';
+  res.writeHead(401, {
+    'Content-Type': 'text/plain',
+    'Content-Length': message.length
+  });
+  return res.end(message);
+}
 
-      if (!req.headers.cookie) return send401();
+module.exports = (req, res) => {
+  // simple request log
+  console.log(`${req.method} ${req.url}`)
+
+  switch (`${req.method} ${req.url}`) {
+
+    case 'GET /': {
+      return readFile('./index.html', (err, data) => {
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+          'Content-Length': data.length
+        });
+        return res.end(data);
+      });
+    }
+
+    case 'POST /login': {
+      const cookie = sign(userDetails, SECRET);
+      res.writeHead(302,{
+        'Location': '/',
+        'Set-Cookie': `jwt=${cookie}; HttpOnly`
+      });
+      return res.end();
+    }
+
+    case 'POST /logout': {
+      res.writeHead(302, {
+        'Location': '/',
+        'Set-Cookie': 'jwt=0; Max-Age=0'
+      });
+      return res.end();
+    }
+
+    case 'GET /auth_check': {
+      if (!req.headers.cookie) {
+        return send401(res);
+      }
 
       const { jwt } = parse(req.headers.cookie);
 
-      if (!jwt) return send401();
+      if (!jwt) {
+        return send401(res);
+      }
 
       return verify(jwt, SECRET, (err, jwt) => {
         if (err) {
-          return send401();
+          return send401(res);
         } else {
           const message = `Your user id is: ${jwt.userId}`;
-          res.writeHead(
-            200,
-            {
-              'Content-Type': 'text/plain',
-              'Content-Length': message.length
-            }
-          );
+          res.writeHead(200,{
+            'Content-Type': 'text/plain',
+            'Content-Length': message.length
+          });
           return res.end(message);
         }
       });
-    default:
-      res.writeHead(
-        404,
-        {
-          'Content-Type': 'text/html',
-          'Content-Length': notFoundPage.length
-        }
-      );
+    }
+
+    default: {
+      res.writeHead(404,{
+        'Content-Type': 'text/html',
+        'Content-Length': notFoundPage.length
+      });
       return res.end(notFoundPage);
+    }
   }
 }
